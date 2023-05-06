5697845ea85ca8f179c142d1cfccedb8ee65b95c
n-destin <111414464+n-destin@users.noreply.github.com>
2023-05-05 23:28:13 -0400

    this is a testing commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import CountReducer from './count-reducer'
-
+//creating the root for all reducers, from which other 
 const rootReducer = combineReducers({
-    count:CountReducer
+    count:CountReducer,
 })
-
 
 export default rootReducer
